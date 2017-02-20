ef6b61a26a645b9dc74399fc682db6ec33a71ca7
Eric Lobdell <eric@ericlobdell.me>
2017-02-19 22:06:07 -0500

    Implemented MustInvoke analyzer

--- MustInvokeBaseMethod/MustInvokeBaseMethod/DiagnosticAnalyzer.cs
+++ MustInvokeBaseMethod/MustInvokeBaseMethod/DiagnosticAnalyzer.cs
@@ -13,38 +13,56 @@ namespace MustInvokeBaseMethod
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class MustInvokeBaseMethodAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "MustInvokeBaseMethod";
+        public static string DiagnosticId = "MustInvokeBaseMethod";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private static DiagnosticDescriptor rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            "Find overriden methods that do not call the base class' method", 
+            "Virtual methods with [MustInvoke] must be invoked in overrides", 
+            "Usage", 
+            DiagnosticSeverity.Error, 
+            true);
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => new ImmutableArray<DiagnosticDescriptor> { rule };
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction<SyntaxKind>(this.AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var method = context.Node as MethodDeclarationSyntax;
+            var model = context.SemanticModel;
+            var methodSymbol = model.GetDeclaredSymbol(method) as IMethodSymbol;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            if (methodSymbol.IsOverride)
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                var overridenMethod = methodSymbol.OverriddenMethod;
+                var hasAttribute = overridenMethod
+                    .GetAttributes()
+                    .Any(a => a.AttributeClass.Name == "MustInvokeAttribute");
 
-                context.ReportDiagnostic(diagnostic);
+                context.CancellationToken.ThrowIfCancellationRequested();
+
+                if(hasAttribute)
+                {
+                    var invocations = method.DescendantNodes(_ => true)
+                        .OfType<InvocationExpressionSyntax>();
+
+                    foreach (var invocation in invocations)
+                    {
+                        var invocationSymbol = model.GetSymbolInfo(invocation.Expression).Symbol as IMethodSymbol;
+
+                        if (invocationSymbol == overridenMethod)
+                            return;
+
+                        context.ReportDiagnostic(Diagnostic.Create(rule, method.Identifier.GetLocation()));
+                    }
+                }
+
             }
         }
     }
---
+++ MustInvokeBaseMethod/MustInvokeBaseMethod/MustInvokeBaseMethodCallMethodCodeFix.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MustInvokeBaseMethod
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp)]
+    [Shared]
+    class MustInvokeBaseMethodCallMethodCodeFix : CodeFixProvider
+    {
+        private const string codeFixDescription = "Add base invocation";
+        public override ImmutableArray<string> FixableDiagnosticIds =>
+            new ImmutableArray<string> { MustInvokeBaseMethodAnalyzer.DiagnosticId };
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken)
+                .ConfigureAwait(false);
+
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            var diagnostic = context.Diagnostics[0];
+            var methodNode = root.FindNode(diagnostic.Location.SourceSpan) as MethodDeclarationSyntax;
+            var model = await context.Document.GetSemanticModelAsync(context.CancellationToken);
+            var methodSybol = model.GetDeclaredSymbol(methodNode) as IMethodSymbol;
+            var invocation = CreateInvocation(methodSybol);
+
+            invocation = AddArguments(context, methodSybol, invocation);
+
+            var statement = CreateStatement(context, methodNode, methodSybol, invocation);
+            var newRoot = CreateRoot(root, methodNode, statement);
+
+            context.RegisterCodeFix(CodeAction.Create(codeFixDescription, _ => Task.FromResult(context.Document.WithSyntaxRoot(newRoot)), codeFixDescription), diagnostic);
+        }
+
+        private SyntaxNode CreateRoot(SyntaxNode root, MethodDeclarationSyntax methodNode, StatementSyntax statement)
+        {
+            var body = methodNode.Body;
+            var firstNode = body.ChildNodes().FirstOrDefault();
+            var newBody = firstNode != null ?
+                    body.InsertNodesBefore(body.ChildNodes().First(), new[] { statement }) :
+                    SyntaxFactory.Block(statement);
+
+            var newRoot = root.ReplaceNode(body, newBody);
+
+            return newRoot;
+
+        }
+
+        private StatementSyntax CreateStatement(CodeFixContext context, MethodDeclarationSyntax methodNode, IMethodSymbol methodSybol, InvocationExpressionSyntax invocation)
+        {
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            StatementSyntax statement = null;
+
+            if (!methodSybol.ReturnsVoid)
+            {
+                var returnValueSafeName = CreateSafeLocalVariableName(methodNode, methodSybol);
+                statement = SyntaxFactory.LocalDeclarationStatement(
+                    SyntaxFactory.VariableDeclaration(SyntaxFactory.IdentifierName("var"))
+                    .WithVariables(
+                        SyntaxFactory.SingletonSeparatedList(
+                            SyntaxFactory.VariableDeclarator(
+                                SyntaxFactory.Identifier(returnValueSafeName))
+                            .WithInitializer(SyntaxFactory.EqualsValueClause(invocation)))));
+            }
+            else
+            {
+                statement = SyntaxFactory.ExpressionStatement(invocation);
+            }
+
+            return statement.WithAdditionalAnnotations(Formatter.Annotation);
+        }
+
+        private string CreateSafeLocalVariableName(MethodDeclarationSyntax methodNode, IMethodSymbol methodSymbol)
+        {
+            var localDeclarations = methodNode.DescendantNodes(_ => true)
+                    .OfType<VariableDeclaratorSyntax>();
+
+            var returnValueName = $"{methodSymbol.Name.Substring(0,1).ToLower()}{methodSymbol.Name.Substring(1)}Result";
+            var returnValueSafeName = returnValueName;
+            var returnValueCount = 0;
+
+            while (localDeclarations.Any( _ => _.Identifier.Text == returnValueSafeName))
+            {
+                returnValueSafeName = $"{returnValueName}{returnValueCount}";
+                returnValueCount++;
+            }
+
+            return returnValueSafeName;
+        }
+
+        private static InvocationExpressionSyntax AddArguments(CodeFixContext context, IMethodSymbol methodSybol, InvocationExpressionSyntax invocation)
+        {
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            var argumentCount = methodSybol.Parameters.Length;
+            if (argumentCount > 0)
+            {
+                var arguments = new SyntaxNodeOrToken[(2 * argumentCount) - 1];
+                for (int i = 0; i < argumentCount; i++)
+                {
+                    var parameter = methodSybol.Parameters[i];
+                    var argument = SyntaxFactory.Argument(SyntaxFactory.IdentifierName(parameter.Name));
+
+                    if (parameter.RefKind.HasFlag(RefKind.Ref))
+                    {
+                        argument = argument.WithRefOrOutKeyword(SyntaxFactory.Token(SyntaxKind.RefKeyword));
+
+                    }
+                    else if (parameter.RefKind.HasFlag(RefKind.Out))
+                    {
+                        argument = argument.WithRefOrOutKeyword(SyntaxFactory.Token(SyntaxKind.OutKeyword));
+                    }
+
+                    arguments[2 * i] = argument;
+
+                    if (i < argumentCount - 1)
+                    {
+                        arguments[(2 * i) + 1] = SyntaxFactory.Token(SyntaxKind.CommaToken);
+                    }
+                }
+
+                invocation = invocation.WithArgumentList(
+                    SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList<ArgumentSyntax>(arguments))
+                        .WithOpenParenToken(SyntaxFactory.Token(SyntaxKind.OpenParenToken))
+                        .WithCloseParenToken(SyntaxFactory.Token(SyntaxKind.CloseParenToken)));
+            }
+
+            return invocation;
+        }
+
+        private static InvocationExpressionSyntax CreateInvocation(IMethodSymbol methodSybol)
+        {
+            return SyntaxFactory.InvocationExpression(
+                SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    SyntaxFactory.BaseExpression().WithToken(SyntaxFactory.Token(SyntaxKind.BaseKeyword)),
+                    SyntaxFactory.IdentifierName(methodSybol.Name))
+                .WithOperatorToken(SyntaxFactory.Token(SyntaxKind.DotToken)));
+        }
+    }
+}
